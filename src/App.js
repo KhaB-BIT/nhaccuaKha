d6aa4cf0443a5671479ace701011e84c1213b0da
Minh Kha Bui <buiminhkha24072001@gmail.com>
2022-12-06 21:59:28 +0700

    #2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { useState } from "react";
+
 function App() {
-  return <div className="App">let start</div>;
+  const [value, setValue] = useState("");
+
+  return (
+    <div className="flex gap-8 justify-center items-center h-screen">
+      <input
+        type="text"
+        className="border-blue-500 border outline-none w-[400px] px-4 py-2"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <button className="bg-blue-400 rounded-md px-4 py-2 text-white">
+        Add
+      </button>
+    </div>
+  );
 }
 
 export default App;
